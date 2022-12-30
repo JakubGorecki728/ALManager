fdf501c63232609001122b20c042e94202b3a467
JakubGorecki <kubolen@gmail.com>
2022-12-30 21:56:48 +0100

    added view handling for new components

--- ALManager/frontend/src/app/app-routing.module.ts
+++ ALManager/frontend/src/app/app-routing.module.ts
@@ -7,7 +7,16 @@ import { PostsComponent } from './components/posts/posts.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 
+import { ProductsComponent } from './components/products/products.component';
+import { AssemblyLinesComponent } from './components/assembly-lines/assembly-lines.component';
+import { WorkstationsComponent } from './components/workstations/workstations.component';
+
 const routes: Routes = [
+  { path: "products", component: ProductsComponent, canActivate: [AuthGuard]},
+  { path: "assembly-lines", component: AssemblyLinesComponent, canActivate: [AuthGuard]},
+  { path: "workstations", component: WorkstationsComponent, canActivate: [AuthGuard]},
+
+
   { path: "posts", component: PostsComponent, canActivate: [AuthGuard]},
   { path: "login", component: LoginComponent },
   { path: "signup", component: SignupComponent },
